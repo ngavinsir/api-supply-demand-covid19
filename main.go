858919909a8a50856d03efaf3f8ec0081d445f92
ngavinsir <ngavinsir@gmail.com>
2020-03-27 01:09:01 +0700

    auth: handler

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"github.com/go-chi/chi"
 	_ "github.com/lib/pq"
 	"github.com/ngavinsir/api-supply-demand-covid19/database"
+	"github.com/ngavinsir/api-supply-demand-covid19/handler"
 )
 
 //go:generate sqlboiler --wipe psql
@@ -15,17 +16,20 @@ import (
 func main() {
 	router := chi.NewRouter()
 
-	_, err := database.InitDB()
+	db, err := database.InitDB()
 	if err != nil {
 		panic(err)
 	}
 	log.Println("connected to db")
+
+	api := handler.NewAPI(db)
+	router.Mount("/api/v1", api.Router())
 
 	port := ":4040"
 	if envPort := os.Getenv("PORT"); envPort != "" {
 		port = ":" + envPort
 	}
 
-	log.Printf("Server started on %s", port)
 	log.Fatal(http.ListenAndServe(port, router))
+	log.Printf("Server started on %s", port)
 }
